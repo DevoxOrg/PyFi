1edd104fbb77d626ee6edbbc267d693b41302a0b
ndevox <nick.a.sarbicki@gmail.com>
2015-12-22 16:24:23 +0000

    Setting up parser system for parsing different banks.

---
+++ pyfi/Statement/file_parsers.py
@@ -0,0 +1,27 @@
+import csv
+
+from pyfi.db.session import create_session
+
+class base_parser:
+    """
+    We need individual parsers to deal with different bank formats.
+
+    They all need some common functionality so will inherit from this base class.
+
+    Initial requirements are to create the data fields to insert into and call the parse function which parses the file.
+
+    All parsers should result in the same output so we can then have the insert_into_db method.
+    """
+    def __init__(self, filepath):
+        self.transactions = ()
+        self.file = filepath
+
+        self.parse(self.file)
+
+        self.insert_into_db()
+
+    def parse(self, filepath):
+        pass
+
+    def insert_into_db(self):
+        pass
